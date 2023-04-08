b94b72adf3762f33d0b4b51e0131e0c9cd0cee53
Momin-k <101165202+Momin-k@users.noreply.github.com>
2023-04-08 15:04:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ var specialsPromise = MenuDataService.getMenuItemsForCategory('L');
 specialsPromise.then(function (response) {
   ctrl.specials = response.data;
 });
+var categoriesPromise = MenuDataService.getAllCategories();
+categoriesPromise.then(function (response) {
+  var categories = response.data;
+  var randomIndex = Math.floor(Math.random() * categories.length);
+  var randomCategoryShortName = categories[randomIndex].short_name;
+
+  var specialsPromise = MenuDataService.getMenuItemsForCategory(randomCategoryShortName);
+  specialsPromise.then(function (response) {
+    ctrl.specials = response.data;
+  });
+});
+HomeController.$inject = ['MenuDataService'];
+function HomeController(MenuDataService) {
+  var ctrl = this;
+  // ...
+}
+
